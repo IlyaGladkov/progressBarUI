646977f072d7bda405cc8f48792e75606e056649
iliyagladkov <glagkov.iliya@gmail.com>
2025-01-16 13:06:01 +0300

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ const hideSwitch = document.getElementById('hideSwitch')
 
 const progressBar = document.querySelector('progress-bar')
 
+progressValueInput.addEventListener('input', () => {
+    progressValueInput.value = progressValueInput.value > 100 ? 100 : progressValueInput.value
+})
+
 progressValueInput.addEventListener('change', () => {
     progressBar.setAttribute('value', progressValueInput.value)
 })
